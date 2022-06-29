bed2706ce31fe2e09b10a91a96245620775a1908
AdrielDevelop <adrieldchavea20@gmail.com>
2022-06-29 19:40:49 -0300

    Adaptações_minhas

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ function toggleMenu() {
     menuMenor.classList.toggle("active")
     navigation.classList.toggle("active")
 }
+
+// Ativando Dark-Mode
+const dark = document.getElementById("dark")
+
+dark.addEventListener('change', () => {
+    document.body.classList.toggle("darkMod")
+})
+
+//Validação do Formulário
+function enviar(){
+    const nome = document.querySelector("#inputNome")
+    const email = document.querySelector("#inputEmail")
+    const mensagem = document.querySelector("#inputMensagem")
+    
+    const nomeValor = nome.value.trim()
+    const emailValor = email.value.trim()
+
+    if (nomeValor === "" || emailValor === "" ){
+        window.alert("Preencha corretamente!")
+    }
+    else{
+        window.alert("Mensagem enviada com Sucesso!")
+        nome.value = ""
+        email.value = ""
+        mensagem.value = ""
+    }
+}
